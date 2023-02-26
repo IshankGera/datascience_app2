82b7d5920f93bd340263a0c45dc848ddce09cfe0
Ishank Gera <90376163+IshankGera@users.noreply.github.com>
2023-02-26 22:40:02 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Web app using :red[streamlit]")
+st.header(":blue[Ishank Gera] :sunglasses:")
+st.subheader("currently pursuing innomatics data science internship programme")
+st.subheader("Its my first webapp with 2 Dashboards of student marks and student attendance")
+btn_click = st.button("click Me!")
+if btn_click == True:
+    st.subheader("Welcome!! :sunglasses:")
+
+st.snow()
